9a2dc1cb6d78fe2ff4cc8175fa2e89005936c7c4
morsemarthaworks-nail <morse.marthaworks@gmail.com>
2026-04-24 14:00:20 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Martha Cares Nails Experience",
+  description: "Luxury Nail Booking App"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
